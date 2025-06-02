88d44db77aea826553499140346dcd6412ee0f5a
unknown <babunroy987@gmail.com>
2025-06-02 17:43:40 +0530

    adsfasdfadfaf

--- src/PageElements/HomeCollectionPageElements/SampleCollectionSection.jsx
+++ src/PageElements/HomeCollectionPageElements/SampleCollectionSection.jsx
@@ -2,12 +2,12 @@
 import './HomeCollectionCss/SampleCollectionSection.css';
 import curvePng from '../../Assets/curveLine.png'
 
-const sampleCollectionProcess = [
-    { indx:1, title:"Schedule an Appointment", para:"Fill out the form to schedule your home sample collection. Our team will get in touch to confirm your appointment."},
-    { indx:2, title:"Sample Collection", para:"Our certified phlebotomist arrives at your doorstep at the scheduled time, equipped with sterile, single-use kits to collect your samples safely."},
-    { indx:3, title:"Laboratory Analysis", para:"Collected samples are transported under controlled conditions to our NABL-accredited labs for processing."},
-    { indx:4, title:"Receive Results", para:"Access your test results promptly via email or through our secure online portal."}
-]
+// const sampleCollectionProcess = [
+//     { indx:1, title:"Schedule an Appointment", para:"Fill out the form to schedule your home sample collection. Our team will get in touch to confirm your appointment."},
+//     { indx:2, title:"Sample Collection", para:"Our certified phlebotomist arrives at your doorstep at the scheduled time, equipped with sterile, single-use kits to collect your samples safely."},
+//     { indx:3, title:"Laboratory Analysis", para:"Collected samples are transported under controlled conditions to our NABL-accredited labs for processing."},
+//     { indx:4, title:"Receive Results", para:"Access your test results promptly via email or through our secure online portal."}
+// ]
 export default function SampleCollectionSection(){
     return(
         <>
